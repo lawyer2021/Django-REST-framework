bd9d65179a9103042d7bfaf3035484ce0c4f9a71
lawyer <gconsul@mail.com>
2023-01-10 17:54:24 +0300

    Introduction to REST (creating new project/django, setuping DRF and pluging its to django, making app for user, model of user, admin panel and etc.)

--- DRF/planner/users/serializers.py
+++ DRF/planner/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import Users
+
+
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Users
