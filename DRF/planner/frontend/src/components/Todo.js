c2932356f775093c746038d173362c2875cc7ff3
lawyer <gconsul@mail.com>
2023-02-05 19:37:50 +0300

    Routing, SPA working with handle data (without downloading data via API :-( )

--- DRF/planner/frontend/src/components/Todo.js
+++ DRF/planner/frontend/src/components/Todo.js
@@ -3,57 +3,29 @@ import React from 'react'
 const TodoItem = ({item}) => {
     return (
         <tr>
-            <td>
-                {item.id}
-            </td>
-            <td>
-                {item.text}
-            </td>
-            <td>
-                {item.date_created}
-            </td>
-            <td>
-                {item.date_updated}
-            </td>
-            <td>
-                {item.is_active}
-            </td>
-            <td>
-                {item.project_id}
-            </td>
-            <td>
-                {item.user_id}
-            </td></tr>
+            <td>{item.id}</td>
+            <td>{item.text}</td>
+            <td>{item.date_created}</td>
+            <td>{item.date_updated}</td>
+            <td>{item.is_active}</td>
+            <td>{item.project_id}</td>
+            <td>{item.user_id}</td>
+        </tr>
     )
 }
-
 const TodoList = ({items}) => {
     return (
         <table>
             <tr>
-                <th>
-                    ID
-                </th>
-                <th>
-                    TEXT
-                </th>
-                <th>
-                    DATE CREATED
-                </th>
-                <th>
-                    DATE UPDATED
-                </th>
-                <th>
-                    ACTIVE
-                </th>
-                <th>
-                    PROJECT ID
-                </th>
-                <th>
-                    USER ID
-                </th>
+                <th>ID</th>
+                <th>TEXT</th>
+                <th>DATE CREATED</th>
+                <th>DATE UPDATED</th>
+                <th>ACTIVE</th>
+                <th>PROJECT ID</th>
+                <th>USER ID</th>
             </tr>
-            {items.map((item) => <TodoItem item={item} />)}
+            {items.map((item) => <TodoItem item={item}/>)}
         </table>
     )
 }
